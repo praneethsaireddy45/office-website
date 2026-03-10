12c6a69989b6cf55450ef3cfc780efe42e805cf2
praneethsaireddy45 <praneethsaireddy45@gmail.com>
2026-03-10 16:46:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
+from flask import flask,render_template,request
+import sqlite3
 
+app=flask(__name__)
+
+def get_db():
+  conn=sqlite3.connect("employees.db")
+  conn.row_factory= sqlite3.row
+  return conn
+
+@approute("/",methods=["GET","POST"])
+def index():
+  conn=get_db()
+  if request.method == "post":
+    name=request.form["name"]
+    salary=request.form["salary"]
+    department=request.form["department"]
+    conn.execute("INSERT TNTO employees(name,salary,department) values(psr,1000,support),(name,salary,department))
+    conn.commit()
+
+
+  employees=conn.excute("SELECT* FROM employees").fetchall()
+  conn.close()
+  return render_template("index.html", employees=employees)
+if __name__ == "__main__":
+  app.run(debug=True)
+
+
+  
+
+
+          
